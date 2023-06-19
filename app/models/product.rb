70db037e195f2a996270554f7841da8759ad4552
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-06-19 19:50:48 -0400

    127

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true, length: { minimum: 10, maximum: 50}
@@ -6,5 +10,14 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpeg|jpg|png)\z}i,
     message: 'Must be a URL for GIF, JPG or PNG'
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_items
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 
 end
